06e1ea3985a32d8bc1c2b0fe040d2f28a75105f2
Andrew Kephart <andrew.kephart@alereon.com>
2017-05-15 16:57:28 -0500

    Define opcodes as 8-bit op ids

--- avmm/avmm_data.h
+++ avmm/avmm_data.h
@@ -35,36 +35,40 @@ typedef enum {
  * Enumerate the supported opcodes
  */
 typedef enum {
-    AVM_OP_NOP = 0x00000000, 
-    AVM_OP_STOR = 0x00010000, 
-    AVM_OP_INS = 0x00020000, 
-    AVM_OP_GOTO = 0x00030000, 
-    AVM_OP_JZ = 0x00040000, 
-    AVM_OP_JNZ = 0x00050000, 
-    AVM_OP_FORK = 0x00060000, 
-    AVM_OP_KILL = 0x00070000, 
-    AVM_OP_PUSH = 0x00080000, 
-    AVM_OP_POP = 0x00090000, 
-    AVM_OP_WIDTH = 0x000A0000, 
+    AVM_OP_NOP = 0x00,
+    AVM_OP_STOR = 0x01,
+    AVM_OP_INS = 0x02,
+    AVM_OP_GOTO = 0x03,
+    AVM_OP_JZ = 0x04,
+    AVM_OP_JE = 0x05,
+    AVM_OP_JNZ = 0x06,
+    AVM_OP_FORK = 0x07,
+    AVM_OP_KILL = 0x08,
+    AVM_OP_PUSH = 0x09,
+    AVM_OP_POP = 0x0A,
+    AVM_OP_LABEL = 0x0B,
 
-    AVM_OP_ADD = 0x00100000, 
-    AVM_OP_SUB = 0x00110000, 
-    AVM_OP_MUL = 0x00120000, 
-    AVM_OP_DIV = 0x00130000, 
-    AVM_OP_POW = 0x00140000, 
-    AVM_OP_OR = 0x00150000, 
-    AVM_OP_AND = 0x00160000, 
-    AVM_OP_CMP = 0x00170000, 
-    AVM_OP_INC = 0x00180000,
-    AVM_OP_DEC = 0x00190000,
+    AVM_OP_ADD = 0x0C,
+    AVM_OP_SUB = 0x0D,
+    AVM_OP_MUL = 0x0E,
+    AVM_OP_DIV = 0x0F,
+    AVM_OP_POW = 0x10,
+    AVM_OP_OR = 0x11,
+    AVM_OP_AND = 0x12,
+    AVM_OP_CMP = 0x13,
+    AVM_OP_INC = 0x14,
+    AVM_OP_DEC = 0x15,
 
-    AVM_OP_FILE = 0x00200000, 
-    AVM_OP_IN = 0x00210000, 
-    AVM_OP_OUT = 0x00220000, 
-    AVM_OP_SIZE = 0x00230000,
+    AVM_OP_FILE = 0x16,
+    AVM_OP_IN = 0x17,
+    AVM_OP_OUT = 0x18,
+    AVM_OP_SIZE = 0x19,
 
-    AVM_OP_LABEL = 0x00FF0000,
-    AVM_OP_INVALID = 0x00FFFFFF
+    AVM_OP_INVALID = 0xFF
+
 } avm_opcode_t;
+
+    
+
 
 #endif /* _AVMM_DATA_H_ */
